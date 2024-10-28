c53d9fb77e95a5f8a5e0913527217c9af9da6b4a
Sandeep Patel <52149614+cs2sandeep@users.noreply.github.com>
2024-10-28 13:22:12 +0530

    Format and tiny refactors and comment

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -21,16 +21,17 @@ public class StringCalculator {
             return Integer.parseInt(numbers);
         }
 
-        // If optional delimiter provided
-        // extract out the first line from the given argument
+        // If optional delimiter(s) provided
+        // extract out the delimiter(s) and remove the delimiter line from the given string
         List<String> delimiters = extractDelimiters(numbers);
-        if(numbers.startsWith("//")) {
+        if (numbers.startsWith("//")) {
             numbers = numbers.split("\\n", 2)[1];
         }
 
-        // flag for even-indices sum or odd-indices sum
+        // Flag for even-indices sum or odd-indices sum
+        // Remove delimiters of 0 or 1, as they are not intended as flags not delimiters
         String sumFlag = "";
-        if(delimiters.size() > 0 && delimiters.get(0).equals("0")) {
+        if (delimiters.size() > 0 && delimiters.get(0).equals("0")) {
             sumFlag = "0";
             delimiters.remove(0);
         } else if (delimiters.size() > 0 && delimiters.get(0).equals("1")) {
@@ -38,9 +39,7 @@ public class StringCalculator {
             delimiters.remove(0);
         }
 
-        int sum = 0;
-
-        // When delimiter separated multiple numbers
+        // When delimiter separated multiple numbers, swap each delimiter with comma
         String splitPattern = "[\\n,]";
         if (delimiters.size() > 0) {
             for (String delimiter : delimiters) {
@@ -48,25 +47,17 @@ public class StringCalculator {
             }
         }
         String[] individualNums = numbers.split(splitPattern);
-
-        List<Integer> numsInIntegerFormat = Arrays.stream(individualNums).map(numInStringFormat -> Integer.parseInt(numInStringFormat)).toList();
-
+        List<Integer> numsInIntegerFormat = Arrays.stream(individualNums).map(Integer::valueOf)
+            .collect(Collectors.toList());
 
         // Negative number check
         validateForNegativesReturnNegativesAsStringAndThrow(numsInIntegerFormat);
 
-//        for (String individualNum : individualNums) {
-//            int iN = Integer.parseInt(individualNum);
-//            if (iN > 1000) {    // Ignore number greater than 1000 in the sum
-//                continue;
-//            }
-//            sum += iN;
-//        }
-
+        // Sum
         int index = 0;
         int increment = 1;
 
-        if(sumFlag == "0") {
+        if (sumFlag == "0") {
             index = 0;
             increment = 2;
         } else if (sumFlag == "1") {
@@ -74,21 +65,22 @@ public class StringCalculator {
             increment = 2;
         }
 
-        for(; index < numsInIntegerFormat.size(); index += increment) {
-            if(numsInIntegerFormat.get(index) > 1000) {
+        int sum = 0;
+        for (; index < numsInIntegerFormat.size(); index += increment) {
+            if (numsInIntegerFormat.get(index) > 1000) {
                 continue;
             }
             sum += numsInIntegerFormat.get(index);
         }
-
         return sum;
     }
 
-    private static void validateForNegativesReturnNegativesAsStringAndThrow(List<Integer> numsInIntegerFormat) {
-        List<Integer> negativeNumbers = numsInIntegerFormat.stream().filter(num -> num < 0).toList();
+    private static void validateForNegativesReturnNegativesAsStringAndThrow(
+        List<Integer> numsInIntegerFormat) {
+        List<Integer> negativeNumbers = numsInIntegerFormat.stream().filter(num -> num < 0)
+            .toList();
         if (negativeNumbers.size() > 0) {
-            throw new IllegalArgumentException("negative numbers not allowed " + negativeNumbers.stream().map(String::valueOf).collect(
-                Collectors.joining(",")) );
+            throw new IllegalArgumentException("negative numbers not allowed " + negativeNumbers.stream().map(String::valueOf).collect(Collectors.joining(",")));
         }
     }
 
@@ -99,14 +91,19 @@ public class StringCalculator {
             String[] firstLineAndRest = fullDelimiterString.split("\\n", 2);
             String firstLine = firstLineAndRest[0];
 
-            String firstLineWithoutSlashes = firstLine.substring(2); // After removing first two slashes
+            String firstLineWithoutSlashes = firstLine.substring(
+                2); // After removing first two slashes
 
-            if (firstLineWithoutSlashes.contains("][")) { // Multiple types of delimiters, each enclosed in square brackets e.g. [*][%]
-                firstLineWithoutSlashes = firstLineWithoutSlashes.substring(1, firstLineWithoutSlashes.length() - 1); // remove brackets from either end
+            if (firstLineWithoutSlashes.contains(
+                "][")) { // Multiple types of delimiters, each enclosed in square brackets e.g. [*][%]
+                firstLineWithoutSlashes = firstLineWithoutSlashes.substring(1,
+                    firstLineWithoutSlashes.length() - 1); // remove brackets from either end
                 extractedDelimiters.addAll(Arrays.asList(firstLineWithoutSlashes.split("\\]\\[")));
             } else { // Only single kind of delimiter
-                extractedDelimiters.add(firstLineWithoutSlashes); // either just the character like *
-                if (extractedDelimiters.get(0).startsWith("[")) { // or enclosed in [***] for multiple
+                extractedDelimiters.add(
+                    firstLineWithoutSlashes); // either just the character like *
+                if (extractedDelimiters.get(0)
+                    .startsWith("[")) { // or enclosed in [***] for multiple
                     extractedDelimiters.set(0, extractedDelimiters.get(0).replace("[", ""));
                     extractedDelimiters.set(0, extractedDelimiters.get(0).replace("]", ""));
                 }
